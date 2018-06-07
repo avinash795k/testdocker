412dbf070f50ded5fa8f4b69da1776aeef0df38b
root <root@pacts1.fyre.ibm.com>
2018-06-06 23:20:14 -0700

    changes

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->get('/', function (Request $request, Response $response, array $args) {
     //$name = $args['name'];
-    $response->getBody()->write("Hi avinash kumar. I am feeling great");
+    $response->getBody()->write("Hi avinash kumar. I am feeling great. Finally done");
 
     return $response;
 });
